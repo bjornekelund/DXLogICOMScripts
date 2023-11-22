09f3e457a50e8016d30ee0c4a8c649c93d111ba5
Björn Ekelund <bjorn@ekelund.nu>
2023-11-22 18:15:22 +0100

    Update

--- DXLogICOMScripts/ICOM_RxAntIO.cs
+++ DXLogICOMScripts/ICOM_RxAntIO.cs
@@ -13,15 +13,16 @@ namespace DXLog.net
     {
         readonly bool StatusText = true;
 
-        bool[] RxAntEnabled = new bool[2];
+        int[] RxAntIOstatus = new int[2];
+        string[] RxAntIOstatusString = new string [] { "OFF", "A", "B" };
         byte[] CIVcmd = { 0x16, 0x53, 0x00 };
 
         // Executes at DXLog.net start 
         public void Initialize(FrmMain main)
         {
-            // Disable Rx antenna on both radios to create a known starting state
-            SetIcomRxAnt(1, false, main);
-            SetIcomRxAnt(2, false, main);
+            // Disable Rx Antenna I/O on both radios to create a known starting state
+            SetIcomRxAntIO(1, 0x00, main);
+            SetIcomRxAntIO(2, 0x00, main);
         }
 
         // Executes as DXLog.net close down
@@ -32,23 +33,24 @@ namespace DXLog.net
         // Executes at macro invocation
         public void Main(FrmMain main, ContestData cdata, COMMain comMain)
         {
-            // Toggle state of Rx antenna for currently focused radio
-            SetIcomRxAnt(cdata.FocusedRadio, !RxAntEnabled[cdata.FocusedRadio - 1], main);
+            // Step state Rx antenna I/O for currently focused radio
+            int newstate = (RxAntIOstatus[cdata.FocusedRadio - 1] + 1) % 3;
+            SetIcomRxAntIO(cdata.FocusedRadio, newstate, main);
         }
 
-        private void SetIcomRxAnt(int radio, bool enabled, FrmMain main)
+        private void SetIcomRxAntIO(int radio, int state, FrmMain main)
         {
             CATCommon radioobject = radio == 1 ? main.COMMainProvider._radio1Object : main.COMMainProvider._radio2Object;
 
             if (radioobject != null)
             {
-                RxAntEnabled[radio - 1] = enabled;
-                CIVcmd[2] = (byte)(enabled ? 0x01 : 0x02);
+                    RxAntIOstatus[radio - 1] = state;
+                CIVcmd[2] = (byte)state;
                 radioobject.SendCustomCommand(CIVcmd);
 
                 if (StatusText)
                 {
-                    main.SetMainStatusText(string.Format("IcomRxAntIO: Rx antenna I/O {0} on radio {1} with command {2}.", enabled ? "B" : "A", radio, BitConverter.ToString(CIVcmd)));
+                    main.SetMainStatusText(string.Format("IcomRxAntIO: Rx antenna I/O {0} on radio {1} with command {2}.", RxAntIOstatusString[state], radio, BitConverter.ToString(CIVcmd)));
                 }
             }
             else
